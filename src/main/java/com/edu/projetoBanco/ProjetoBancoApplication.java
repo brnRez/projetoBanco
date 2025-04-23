18addefd3c73c70810e3fdb5b7c454d02318f993
Bruno Maciel <maciel2bruno@gmail.com>
2025-04-23 10:39:50 -0300

    Feat: Create Controllers and Exceptions

--- src/main/java/com/edu/projetoBanco/ProjetoBancoApplication.java
+++ src/main/java/com/edu/projetoBanco/ProjetoBancoApplication.java
@@ -1,13 +1,14 @@
 package com.edu.projetoBanco;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class ProjetoBancoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoBancoApplication.class, args);
 	}
-
 }
